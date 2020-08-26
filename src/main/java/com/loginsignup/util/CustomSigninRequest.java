aa2f2bfa7e0f761dc4c38231988067afc5f9c10b
byionline <10815566+byionline@users.noreply.github.com>
2020-08-26 13:45:37 +0530

    Custom Request Response For Login & Registration

---
+++ src/main/java/com/loginsignup/util/CustomSigninRequest.java
@@ -0,0 +1,32 @@
+package com.loginsignup.util;
+
+import javax.validation.constraints.NotBlank;
+
+public class CustomSigninRequest {
+    
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+    
+}
