504de03dc5f30df27d5293b71ec233aa20404753
byionline <10815566+byionline@users.noreply.github.com>
2020-08-26 18:47:21 +0530

    Added Custom Exception

---
+++ src/main/java/com/loginsignup/exceptions/CustomServerErrorException.java
@@ -0,0 +1,23 @@
+package com.loginsignup.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class CustomServerErrorException extends RuntimeException{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public CustomServerErrorException(String message) {
+        super(message);
+    }
+
+    public CustomServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+}
