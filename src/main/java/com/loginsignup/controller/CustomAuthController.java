391fa1316ceee42d29c99921048f20518392415c
byionline <10815566+byionline@users.noreply.github.com>
2020-08-29 19:38:47 +0530

    Added signup in AuthController

---
+++ src/main/java/com/loginsignup/controller/CustomAuthController.java
@@ -0,0 +1,51 @@
+package com.loginsignup.controller;
+import java.net.URI;
+import java.util.Collections;
+import javax.validation.Valid;
+import com.loginsignup.exceptions.CustomServerErrorException;
+import com.loginsignup.models.RoleType;
+import com.loginsignup.models.Roles;
+import com.loginsignup.models.Usermodel;
+import com.loginsignup.repository.UserRepository;
+import com.loginsignup.repository.UserRoleRepository;
+import com.loginsignup.security.CustomJwtTokenProvider;
+import com.loginsignup.util.CustomApiResponse;
+import com.loginsignup.util.CustomSignupRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+@RestController
+@RequestMapping("/api/auth")
+public class CustomAuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserRoleRepository userRoleRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    CustomJwtTokenProvider customJwtTokenProvider;
+    @PostMapping("/signup")
+    public ResponseEntity<?> userRegistration(@Valid @RequestBody CustomSignupRequest customSignupRequest){
+        if(userRepository.exitsByUsername(customSignupRequest.getUsername())){
+            return new ResponseEntity<>(new CustomApiResponse("Username Already Taken.", false), HttpStatus.BAD_REQUEST);
+        }
+        //Now UserRegistration will be Done
+        Usermodel usermodel = new Usermodel(customSignupRequest.getName(), customSignupRequest.getEmailId(), customSignupRequest.getUsername(), customSignupRequest.getPassword());
+        usermodel.setPassword(passwordEncoder.encode(usermodel.getPassword()));
+        Roles roles = userRoleRepository.findByName(RoleType.USER_ROLE).orElseThrow(()-> new CustomServerErrorException("User Role is not set"));
+        usermodel.setRole(Collections.singleton(roles));
+        Usermodel saveUser =userRepository.save(usermodel);
+        URI loc =ServletUriComponentsBuilder.fromCurrentContextPath().path("/users/{username}").buildAndExpand(saveUser.getUsername()).toUri();
+        return ResponseEntity.created(loc).body(new CustomApiResponse("User is registered successfully", true));
+    }
+}
