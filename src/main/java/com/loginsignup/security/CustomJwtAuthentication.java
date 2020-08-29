0d538a848c3880db28806f0393df2886bd8d1d9d
byionline <10815566+byionline@users.noreply.github.com>
2020-08-29 18:54:52 +0530

    Index Renamed

--- src/main/java/com/loginsignup/security/CustomJwtAuthentication.java
+++ src/main/java/com/loginsignup/security/CustomJwtAuthentication.java
@@ -1,28 +1,21 @@
 package com.loginsignup.security;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
 @Component
 public class customJwtAuthentication implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(customJwtAuthentication.class);
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
         // TODO Auto-generated method stub
         logger.error("Unauthorized Error. {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-
     }
-
 }
--- src/main/java/com/loginsignup/security/CustomJwtFilter.java
+++ src/main/java/com/loginsignup/security/CustomJwtFilter.java
@@ -1,18 +1,19 @@
 package com.loginsignup.security;
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 public class customJwtFilter extends OncePerRequestFilter {
     private static final Logger logger =LoggerFactory.getLogger(customJwtFilter.class);
     private customJwtTokenProvider customJwtTokenProvider;
