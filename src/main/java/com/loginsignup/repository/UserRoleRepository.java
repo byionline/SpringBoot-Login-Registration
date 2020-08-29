2ef22dbbed713a7cb40356f9ec9d86108a4a4048
byionline <10815566+byionline@users.noreply.github.com>
2020-08-29 19:05:42 +0530

    Added findByName in UserRepository

--- src/main/java/com/loginsignup/repository/UserRoleRepository.java
+++ src/main/java/com/loginsignup/repository/UserRoleRepository.java
@@ -1,15 +1,10 @@
 package com.loginsignup.repository;
-
-// import java.util.Optional;
-
-// import com.loginsignup.models.RoleType;
+import java.util.Optional;
+import com.loginsignup.models.RoleType;
 import com.loginsignup.models.Roles;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface UserRoleRepository extends JpaRepository<Roles, Long> {
-    // Optional<Roles> findByName(RoleType name);
-    
+    Optional<Roles> findByName(RoleType name);
 }
