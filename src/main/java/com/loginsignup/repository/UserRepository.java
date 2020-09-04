b951ee48d49fee0c59527636d25ab0b0d1e518c1
byionline <10815566+byionline@users.noreply.github.com>
2020-09-04 21:43:50 +0530

    Bug Fix UserRepository

--- src/main/java/com/loginsignup/repository/UserRepository.java
+++ src/main/java/com/loginsignup/repository/UserRepository.java
@@ -1,14 +1,12 @@
 package com.loginsignup.repository;
-// import java.util.List;
+import java.util.List;
 import java.util.Optional;
 import com.loginsignup.models.Usermodel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Usermodel, Long> {
-    // Optional<Usermodel> findByEmailId(String emailId);
     Optional<Usermodel> findByUsername(String username);
-    Boolean exitsByUsername(String username);
-    // Boolean exitsByEmailId(String emailId);
-    // List<Usermodel> findByIdId(List<Long> userIds);
+    boolean existsUsermodelByUsername(String username);
+    boolean existsUsermodelByEmailId(String emailId);
 }
