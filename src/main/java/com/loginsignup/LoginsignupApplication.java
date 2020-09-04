b35b6fdc77ee8716f8d0d1e8811dcf4512263f6d
byionline <10815566+byionline@users.noreply.github.com>
2020-09-04 21:46:07 +0530

    DataTime Zone Setting

--- src/main/java/com/loginsignup/LoginsignupApplication.java
+++ src/main/java/com/loginsignup/LoginsignupApplication.java
@@ -1,8 +1,22 @@
 package com.loginsignup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+LoginsignupApplication.class,
+Jsr310JpaConverters.class
+})
 public class LoginsignupApplication {
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LoginsignupApplication.class, args);
 	}
